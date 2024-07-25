b4ce6a16c87e9affccd0066fb84beed7d9a88688
Jose Luis Gregoretti <116303145+grego1986@users.noreply.github.com>
2024-07-25 20:34:07 -0300

    inicial

---
+++ src/test/java/com/consejo/consejo/ConsejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consejo.consejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
