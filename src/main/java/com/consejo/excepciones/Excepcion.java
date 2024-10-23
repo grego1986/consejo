e9e8a0229cdfc4627ffa9b202f3e9255279f5d50
Jose Luis Gregoretti <116303145+grego1986@users.noreply.github.com>
2024-10-23 15:39:55 -0300

    se agrego error. html y la clase excepcion

---
+++ src/main/java/com/consejo/excepciones/Excepcion.java
@@ -0,0 +1,33 @@
+package com.consejo.excepciones;
+
+public class Excepcion extends Exception{
+
+	
+	/*
+	 * Atributo al que está asociado el error
+	 */
+	private  String atributo;
+	
+	public Excepcion() {
+		super();
+		
+	}
+
+	public Excepcion(String mensaje) {
+		super(mensaje);
+	}
+
+	public Excepcion(String mensaje, String atributo) {
+		super(mensaje);
+		this.atributo=atributo;
+	}
+
+	public String getAtributo() {
+		return atributo;
+	}
+
+	public void setAtributo(String atributo) {
+		this.atributo = atributo;
+	} 
+
+}
